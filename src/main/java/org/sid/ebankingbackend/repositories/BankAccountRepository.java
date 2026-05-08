1da8c4e575651e9709a032b15a0defdd0f0c527a
hichamouaouche <hichamouaouche@gmail.com>
2026-05-08 02:13:00 +0100

    update

--- src/main/java/org/sid/ebankingbackend/repositories/BankAccountRepository.java
+++ src/main/java/org/sid/ebankingbackend/repositories/BankAccountRepository.java
@@ -5,4 +5,6 @@ import org.sid.ebankingbackend.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BankAccountRepository extends JpaRepository<BankAccount, String> {
+
+
 }
